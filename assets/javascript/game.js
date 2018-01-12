1086ba3de8f76d4b3a5b26ec5811c21329b378b5
dkwazhere <dhealth77@gmail.com>
2018-01-12 02:37:54 -0800

    made buttons work, and search bar working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,72 @@
 $(document).ready(function(){
-  //****************************************************
 // Declaring global variables
-//******************************************************
 var topics = ["Motorcycle", "Surfing", "Beer"];
-//******************************************************
 // Declaring functions
-//******************************************************
-    function createButton(){
-        for (i=0; i<topics.length; i++){
-            $(".buttons").append($("<button>" + topics[i]+ "</button>"));
-        }
+    function motoButtons(){
+        $("#moto").on("click", function(){
+            event.preventDefault();
+            var a = "https://api.giphy.com/v1/gifs/search?q=Motorcycle&api_key=CZ8vxp5Pfp9BIkVCEdK9Mu5Sg6FC9HW6";
+            $.ajax({
+              url: a,
+              method: "GET"
+            }).done(function(response){
+                for (k=0; k<10; k++){
+                console.log(topics);
+                $("#container").append("<img src='"+response.data[k].images.fixed_height.url+"'></img>");
+                }
+            })
+        })
     }
-    $("#submit").on("click", function(){
+    function surfButtons(){
+        $("#surf").on("click", function(){
+            event.preventDefault();
+            var b = "https://api.giphy.com/v1/gifs/search?q=Surfing&api_key=CZ8vxp5Pfp9BIkVCEdK9Mu5Sg6FC9HW6";
+            $.ajax({
+              url: b,
+              method: "GET"
+            }).done(function(response){
+                for (k=0; k<10; k++){
+                console.log(topics);
+                $("#container").append("<img src='"+response.data[k].images.fixed_height.url+"'></img>");
+                }
+            })
+        })
+    }
+    function beerButtons(){
+        $("#beer").on("click", function(){
+            event.preventDefault();
+            var c = "https://api.giphy.com/v1/gifs/search?q=Beer&api_key=CZ8vxp5Pfp9BIkVCEdK9Mu5Sg6FC9HW6"
+            $.ajax({
+              url: c,
+              method: "GET"
+            }).done(function(response){
+                    for (k=0; k<10; k++){
+                    console.log(topics);
+                    $("#container").append("<img src='"+response.data[k].images.fixed_height.url+"'></img>");
+                    }
+            })
+        })
+    }
+    function onClick(){
+        $("#submit").on("click", function(){
         event.preventDefault();
         var input = $("#search").val().trim();
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=CZ8vxp5Pfp9BIkVCEdK9Mu5Sg6FC9HW6";
         topics.push(input);
         $(".buttons").append("<button>" + input + "</button>");
-//********************************************************
-// Ajax
-//********************************************************
         $.ajax({
           url: queryURL,
-          method: 'GET'
+          method: "GET"
         }).done(function(response){
-            console.log(queryURL);
-            console.log(input);
-      })
-  })
-// Calling functions
-    createButton();
+                for (k=0; k<10; k++){
+                console.log(topics);
+                $("#container").append("<img src='"+response.data[k].images.fixed_height.url+"'></img>");
+                }
+            })
+        })
+    };
+    onClick();
+    motoButtons();
+    surfButtons();
+    beerButtons();
 });
