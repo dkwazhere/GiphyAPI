ddc2be76c3a51586c4162bd857fbfafc5e321f96
dkwazhere <dhealth77@gmail.com>
2018-01-13 23:12:56 -0800

    made big changes to clean up the code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,72 +1,51 @@
 $(document).ready(function(){
 // Declaring global variables
-var topics = ["Motorcycle", "Surfing", "Beer"];
-// Declaring functions
-    function motoButtons(){
-        $("#moto").on("click", function(){
-            event.preventDefault();
-            var a = "https://api.giphy.com/v1/gifs/search?q=Motorcycle&api_key=CZ8vxp5Pfp9BIkVCEdK9Mu5Sg6FC9HW6";
-            $.ajax({
-              url: a,
-              method: "GET"
-            }).done(function(response){
-                for (k=0; k<10; k++){
-                console.log(topics);
-                $("#container").append("<img src='"+response.data[k].images.fixed_height.url+"'></img>");
-                }
-            })
-        })
-    }
-    function surfButtons(){
-        $("#surf").on("click", function(){
-            event.preventDefault();
-            var b = "https://api.giphy.com/v1/gifs/search?q=Surfing&api_key=CZ8vxp5Pfp9BIkVCEdK9Mu5Sg6FC9HW6";
-            $.ajax({
-              url: b,
-              method: "GET"
-            }).done(function(response){
-                for (k=0; k<10; k++){
-                console.log(topics);
-                $("#container").append("<img src='"+response.data[k].images.fixed_height.url+"'></img>");
-                }
-            })
-        })
-    }
-    function beerButtons(){
-        $("#beer").on("click", function(){
-            event.preventDefault();
-            var c = "https://api.giphy.com/v1/gifs/search?q=Beer&api_key=CZ8vxp5Pfp9BIkVCEdK9Mu5Sg6FC9HW6"
-            $.ajax({
-              url: c,
-              method: "GET"
-            }).done(function(response){
-                    for (k=0; k<10; k++){
-                    console.log(topics);
-                    $("#container").append("<img src='"+response.data[k].images.fixed_height.url+"'></img>");
-                    }
-            })
-        })
-    }
-    function onClick(){
-        $("#submit").on("click", function(){
-        event.preventDefault();
-        var input = $("#search").val().trim();
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=CZ8vxp5Pfp9BIkVCEdK9Mu5Sg6FC9HW6";
-        topics.push(input);
-        $(".buttons").append("<button>" + input + "</button>");
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).done(function(response){
-                for (k=0; k<10; k++){
-                console.log(topics);
-                $("#container").append("<img src='"+response.data[k].images.fixed_height.url+"'></img>");
-                }
-            })
-        })
+var topics = ["Motorcycle", "Surfing", "Beer", "Kobe"];
+
+    function createButtons(){
+        $("#buttons").empty();
+        for (i=0; i<topics.length; i++){
+            var buttons = $("<button>" + topics[i] + "</button>");
+            buttons.attr("data-topic", topics[i]);
+            buttons.addClass("buttons");
+            $("#buttons").append(buttons);
+        };
+        gif();
     };
-    onClick();
-    motoButtons();
-    surfButtons();
-    beerButtons();
+    createButtons();
+    function gif(){
+        $(".buttons").on("click", function(){
+        $("#container").empty();
+            var input = $(this).attr("data-topic");
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=CZ8vxp5Pfp9BIkVCEdK9Mu5Sg6FC9HW6";
+            $.ajax({
+              url: queryURL,
+              method: "GET"
+            }).done(function(response){
+                console.log(queryURL);
+                console.log(response);
+                var data = response.data;
+                for (i=0; i<10; i++){
+                    var topicDiv = $("<div>");
+                    // var p = $("<p>").text("Rating: " + data[i].rating);
+                    var rating = data[i].rating;
+                    var topicImage = $("<img>");
+                    topicImage.attr("src", data[i].images.fixed_height.url);
+                    topicImage.attr("data-state", data[i].images.fixed_height.url)
+                    topicImage.attr("data-still", data[i].images.fixed_height_still.url)
+                    topicDiv.append(rating);
+                    topicDiv.append(topicImage);
+                    $("#container").prepend(topicImage);
+                    $("#container").prepend("<br> Rating: " + rating);
+                }
+            })
+        })
+    }
+    $("#submit").on("click", function(){
+        event.preventDefault();
+        $("#container").empty();
+        var userClick = $("#search").val().trim();
+        topics.push(userClick);
+        createButtons();
+    })
 });
